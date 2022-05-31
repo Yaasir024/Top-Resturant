91e93467c1f0fe29c2b20b3926bd4b3617105302
Yaasir024 <yope4ever@gmail.com>
2022-05-31 14:57:17 +0100

    /restructuring paths

--- components/restaurant/Row.vue
+++ components/restaurant/Row.vue
@@ -1,11 +1,15 @@
 <template>
-<div class="row" v-if="isHeader">
+  <div class="row" v-if="isHeader">
     <h4 class="header">Rank</h4>
     <h4 class="header">Chain</h4>
-</div>
-  <div v-else class="row" :style="index % 2 == 0 ? {background: 'rgba(128,128,128,0.15)'} : null">
+  </div>
+  <div
+    v-else
+    class="row"
+    :style="index % 2 == 0 ? { background: 'rgba(128,128,128,0.15)' } : null"
+  >
     <h4 class="header rank">{{ rank }}</h4>
-    <a :href="`/restaurant/${name}`" class="header link">{{ name }}</a>
+    <a :href="`/restaurants/${name}`" class="header link">{{ name }}</a>
   </div>
 </template>
 
